fc0a97e2812e1ddd9e38c462e57c66d334693ca4
Jason Sznol <jason@nimbus.co>
2026-04-01 08:43:11 -0400

    Update Gradle to 9.4.1

---
+++ samples/android/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.dsl.*
+
+plugins {
+    alias(libs.plugins.android.app)
+}
+
+android {
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.adsbynimbus.dynamicprice.sample"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+        manifestPlaceholders["appName"] = "Nimbus Dynamic Price"
+        with(providers) {
+            manifestPlaceholders["gamAppId"] = gradleProperty("sample.admanager.appid").get()
+            buildConfigField("String", "API_KEY", "\"${gradleProperty("sample.nimbus.apikey").get()}\"")
+            buildConfigField("String", "PUBLISHER_KEY", "\"${gradleProperty("sample.nimbus.publisherkey").get()}\"")
+            buildConfigField("String", "ADMANAGER_ADUNIT_ID", "\"${gradleProperty("sample.admanager.adunitid").get()}\"")
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin.target.compilations.configureEach {
+    compileTaskProvider.configure {
+        compilerOptions.jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+
+}
